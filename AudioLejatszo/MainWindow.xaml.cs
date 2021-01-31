8ed570b754945b11500c42d570b581759b7d9470
StrikerElite <strikerelite@outlook.hu>
2021-01-31 13:08:51 +0100

    Repo Cleanup, Működő Fájl Kiválasztás

--- AudioLejatszo/MainWindow.xaml.cs
+++ AudioLejatszo/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using Microsoft.Win32;
-using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AudioLejatszo
 {
@@ -21,12 +12,13 @@ namespace AudioLejatszo
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> openFilePaths = new List<string>();
+        BindingList<string> openFilePaths = new BindingList<string>();
         MediaPlayer mediaPlayer;
 
         public MainWindow()
         {
             InitializeComponent();
+            OpenSongList.ItemsSource = openFilePaths;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -40,10 +32,12 @@ namespace AudioLejatszo
             {
                 Multiselect = true,
                 Filter = "Audio Files (*.mp3; *.flac; *.wav) |*.mp3;*.flac;*.wav"
-            };
-            fileDialog.ShowDialog();
-            fileDialog.FileNames.ToList().ForEach(x => openFilePaths.Add(x));
-            a
+            }; 
+            //Valamilyen csodálatos okból "bool?"-t ad vissza, pedig elvileg nem is lehet null.
+            if ((bool)fileDialog.ShowDialog())
+            {
+                fileDialog.FileNames.ToList().ForEach(x => openFilePaths.Add(x));
+            }
         }
     }
 }
