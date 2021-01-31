ed08abc314d2ad68e0b229da5d4585fc4ff59c21
StrikerElite <strikerelite@outlook.hu>
2021-01-31 16:37:52 +0100

    Basic Song Playing

--- AudioLejatszo/MainWindow.xaml.cs
+++ AudioLejatszo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -12,13 +13,32 @@ namespace AudioLejatszo
     /// </summary>
     public partial class MainWindow : Window
     {
-        BindingList<string> openFilePaths = new BindingList<string>();
-        MediaPlayer mediaPlayer;
+        BindingList<string> _playlist = new BindingList<string>();
+        MediaPlayer _mediaPlayer = new MediaPlayer();
 
         public MainWindow()
         {
             InitializeComponent();
-            OpenSongList.ItemsSource = openFilePaths;
+            OpenSongList.ItemsSource = _playlist;
+            _mediaPlayer.MediaEnded += _mediaPlayer_MediaEnded;
+        }
+
+        private void _mediaPlayer_MediaEnded(object sender, EventArgs e)
+        {
+            if (OpenSongList.SelectedIndex == -1) return;
+
+            OpenSongList.SelectedIndex++;
+            if (OpenSongList.SelectedIndex != OpenSongList.Items.Count-1)
+            {
+                _mediaPlayer.Open(new Uri((string)OpenSongList.SelectedItem));
+                Play();
+            }
+            else
+            {
+                Pause();
+                OpenSongList.SelectedIndex = -1;
+                _mediaPlayer.Stop();
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -36,20 +56,53 @@ namespace AudioLejatszo
             //Valamilyen csodálatos okból "bool?"-t ad vissza, pedig elvileg nem is lehet null.
             if ((bool)fileDialog.ShowDialog())
             {
-                fileDialog.FileNames.ToList().ForEach(x => openFilePaths.Add(x));
+                fileDialog.FileNames.ToList().ForEach(x => 
+                { 
+                    _playlist.Add(x);
+                });
             }
         }
 
         private void PlayStop_Click(object sender, RoutedEventArgs e)
         {
-            if ((string)PlayStop.Content == "Play" && openFilePaths.Count != 0)
+            if ((string)PlayStop.Content == "Play" && _playlist.Count != 0)
             {
-                PlayStop.Content = "Stop";
+                Play();
             }
-            else if ((string)PlayStop.Content == "Stop")
+            else if ((string)PlayStop.Content == "Pause")
             {
-                PlayStop.Content = "Play";
+                Pause();
             }
         }
+
+        private void OpenSongList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (OpenSongList.SelectedIndex == -1) return;
+            try
+            {
+                _mediaPlayer.Open(new Uri((string)OpenSongList.SelectedItem));
+                Play();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Nem lehet lejátszani a kiválasztott fájlt!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        void Play()
+        {
+            if (OpenSongList.SelectedIndex == -1) return;
+
+            PlayStop.Content = "Pause";
+            _mediaPlayer.Play();
+        }
+
+        void Pause()
+        {
+            PlayStop.Content = "Play";
+            _mediaPlayer.Pause();
+        }
+
+        
     }
 }
